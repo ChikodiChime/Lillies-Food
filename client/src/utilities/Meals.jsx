3b393425b5cef8035aabb77a0c99e80a1d3654af
--global <chikkychime@gmail.com>
2024-01-31 21:41:07 +0100

    Dashboard utils added

---
+++ client/src/utilities/Meals.jsx
@@ -0,0 +1,56 @@
+export const Meals = [
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    },
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    },
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    },
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    },
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    },
+    {
+        img: 'img3',
+        name: 'Stir Fry Pasta',
+        summary: 'Stir fry pasta yada yada because of season',
+        details:'Get this plate of stirfry to intrigue your tastebuds and make you salivate. Ever looked at a plate od food and thought - I want you in me ? and when you have it in you, it has you all wet in your mouth and your belly is excited about the downpour of food it is about to receive?? lol, I have said more than enough. Get this plate and have the blissful satisfaction that is good food.',
+        price: 1000,
+        prepTime: '10-20 mins',
+        stock: 10
+    }
+]
