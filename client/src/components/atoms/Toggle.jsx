1dcbf4df09dce78aa51685d864745a672e770098
--global <chikkychime@gmail.com>
2024-02-01 18:08:15 +0100

    Light and Dark mode toggle added

---
+++ client/src/components/atoms/Toggle.jsx
@@ -0,0 +1,18 @@
+
+const Toggle = ({handleChange, isChecked}) => {
+  return (
+    <div className="absolute top-8 right-8">
+        <input
+         type="checkbox" 
+         name="" 
+         id="check"
+         className="toggle hidden "
+         onChange={handleChange}
+         checked={isChecked} 
+         />
+         <label htmlFor="check" className="togLabel flex cursor-pointer items-center text-2xl">Darkmode</label>
+    </div>
+  )
+}
+
+export default Toggle
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { UserContext } from '../../context/userContext'
+import useLocalStorage from 'use-local-storage'
 import SideBar from "src/components/molecules/User/SideBar";
+import Toggle from "src/components/atoms/Toggle";
 
 
 const Dashboard = () => {
-  // const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useLocalStorage('isDark', false);
     const {user} = useContext(UserContext)
   return (
-    <div className="dashboard text-black" data-theme='dark'>
+    <div className="dashboard text-black" data-theme={isDark ? "dark" : "light"}>
+      <Toggle
+      isChecked={isDark}
+      handleChange={() => setIsDark(!isDark) }
+      />
         <h1>Dashboard</h1>
         { !!user && (<h2>Hi {user.name} !</h2>) }
         <SideBar/>
