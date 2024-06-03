742b47b0e168a820f72a9aea6ce79cb21ad71075
--global <chikkychime@gmail.com>
2024-06-03 15:44:25 +0100

    CORS updated

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const {mongoose} = require('mongoose');
-const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose'); // No need to destructure mongoose
+const cookieParser = require('cookie-parser');
 const app = express();
-const cartRoutes = require('./routes/Cart')
-const mealRoutes = require('./routes/mealRoutes')
-//db connection
+const cartRoutes = require('./routes/Cart');
+const mealRoutes = require('./routes/mealRoutes');
+
+// db connection
 mongoose.connect(process.env.MONGO_URL)
-.then(() =>console.log('Database Connected'))
-.catch((err) => console.log('Database not connected ', err))
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log('Database not connected', err));
 
 const allowedOrigins = ['https://lillies-food-to43.vercel.app'];
 
@@ -20,20 +21,26 @@ const corsOptions = {
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  credentials: true, // Enable credentials (cookies, authorization headers, TLS client certificates)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow these headers
+  preflightContinue: false,
+  optionsSuccessStatus: 204 // Respond with status 204 for OPTIONS requests
 };
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
+
 // Middleware for parsing JSON bodies
 app.use(express.json({ limit: '10mb' }));
 
 // Middleware for parsing URL-encoded bodies
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use('/', require('./routes/authRoutes'))
+app.use('/', require('./routes/authRoutes'));
 app.use('/api/cart', cartRoutes);
-app.use('/api/meal', mealRoutes)
+app.use('/api/meal', mealRoutes);
 
 const port = 8000;
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
