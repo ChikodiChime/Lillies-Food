75442ddd79c074d1bf55d2dbaf7a0d245dc80d7b
--global <chikkychime@gmail.com>
2024-06-03 16:35:32 +0100

    url updated

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -9,7 +9,7 @@ export function UserContextProvider({ children }) {
 
   useEffect(() => {
     if (!user) {
-      axios.get('https://lillies-food-psi.vercel.app/profile')
+      axios.get('/profile')
         .then(({ data }) => {
           setUser(data);
         })
